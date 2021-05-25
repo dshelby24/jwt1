a878d086a56958892114a4983feae5211e4da886
domynique <dshelbyv@gmail.com>
2021-05-25 16:05:59 -0400

    new

---
+++ controllers/authController.js
@@ -0,0 +1,13 @@
+
+module.exports.signup_get = (req, res) => {
+res.render('signup')
+}
+module.exports.login_get = (req, res) => {
+    res.render('login')
+}
+module.exports.signup_post = async (req, res) => {
+    res.render('new signup')
+}
+module.exports.login_post = async (req, res) => {
+    res.render('user login')
+}
